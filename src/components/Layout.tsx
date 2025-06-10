5c9c77509026283cb9e75fe900ee2f4e9d728e2a
Jihoon <godpd440@naver.com>
2025-06-10 18:09:18 +0900

    error check

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 }
 
 export default function Layout({ children }: LayoutProps) {
-  const { data: session } = useSession()
+  // const { data: session } = useSession()
 
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
